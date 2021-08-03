8a210fcc142868c78d5c033fcd451a9dd9774c7c
zoner52 <Fcfnjdf%13>
2021-08-03 13:04:57 +0300

    final?

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,10 @@
 package koschei.models;
 
+
+import org.springframework.stereotype.Component;
+@Component
 public class Deth8 {
+
+    @Override
+    public String toString() {return "";}
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
 
+    private Deth8 deth8;
+
+    @Autowired
+    public Needle7 (Deth8 deth8) {
+        this.deth8 = deth8;
+    }
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth8.toString();
     }
 }
